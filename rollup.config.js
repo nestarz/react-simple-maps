5376a6bf75ae82479965b0222fa0993b4fd3b5c4
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-10-13 14:39:25 +0200

    add minifier

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,26 @@
 import VuePlugin from 'rollup-plugin-vue';
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json"
 
-export default [{
+const esm = (minify) => ({
   input: "src/index.js",
   output: [
     {
-      file: pkg.module,
+      file: minify ? `${pkg.module.split('.js')[0]}.min.js` : pkg.module,
       format: "es"
     },
   ],
   plugins: [
     VuePlugin(),
     commonjs(),
+    minify ? terser() : null,
     resolve({
       mainFields: ['module'],
     })
-  ],
-}]
+  ]
+});
+
+export default [esm(true), esm(false)]
